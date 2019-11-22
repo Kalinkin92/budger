dc5f354ad14f94ae44fccd682bb37502e3f6c94c
Igor Kalinkin <i.kalinkin@cartat.ru>
2019-11-22 10:48:29 +0300

    Updates

---
+++ precache-manifest.f3be6e039d578fb087fdff87842931d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdabed4944a6da2ed20b6ab7a681592b",
+    "url": "/budger/index.html"
+  },
+  {
+    "revision": "413842c8270ab70f6a35",
+    "url": "/budger/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d0a4656f135f459ba2ab",
+    "url": "/budger/static/css/main.35039639.chunk.css"
+  },
+  {
+    "revision": "413842c8270ab70f6a35",
+    "url": "/budger/static/js/2.c4a1f372.chunk.js"
+  },
+  {
+    "revision": "d0a4656f135f459ba2ab",
+    "url": "/budger/static/js/main.fac98712.chunk.js"
+  },
+  {
+    "revision": "845ea9f101dae58d4eee",
+    "url": "/budger/static/js/runtime-main.e6dfec19.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/budger/static/media/logo.25bf045c.svg"
+  }
+]);
